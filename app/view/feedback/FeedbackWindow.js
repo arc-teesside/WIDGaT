867da398b4174b4e6824f73637d4ffb3f7e02ec1
Franck <F.Perrin@tees.ac.uk>
2012-11-06 19:55:19 +0000

    + Feedback form w/ ReCaptcha + Feedback logged in fdbck.txt + Php form and captcha lib + Call to webservices for style panel
    Updated video guide link

--- app/view/feedback/FeedbackWindow.js
+++ app/view/feedback/FeedbackWindow.js
@@ -22,7 +22,60 @@ Ext.define('WIDGaT.view.feedback.FeedbackWindow' ,{
 				'->', // greedy spacer so that the buttons are aligned to each side
 				{
 					id: 'feedbackSend',
-					text: 'Send'
+					text: 'Send',
+					handler: function() {
+						var form = this.up('window').down('form').getForm();
+						var win = this.up('window');
+						if(form.isValid()) {
+							fieldValues = form.getFieldValues()
+							fieldValues.recaptcha_response_field = this.up('window').down('recaptcha').getResponse();
+							fieldValues.recaptcha_challenge_field = this.up('window').down('recaptcha').getChallenge();
+							console.log(fieldValues);
+							
+							form.submit({
+								clientValidation: true,
+								url: 'fdbck.php',
+								params: fieldValues,
+								success: function(form, action) {
+									Ext.Msg.alert('Thank you!', 'Thank you, your message has been successfuly sent. We will get back to you as soon as possible.');
+									win.close();
+								},
+								failure: function(form, action) {
+									switch (action.failureType) {
+										case Ext.form.action.Action.CLIENT_INVALID:
+											Ext.Msg.alert('Failure', 'Form fields may not be submitted with invalid values');
+											break;
+										case Ext.form.action.Action.CONNECT_FAILURE:
+											Ext.Msg.alert('Failure', 'Ajax communication failed');
+											break;
+										case Ext.form.action.Action.SERVER_INVALID:
+										   Ext.Msg.alert('Failure', action.result.msg);
+								   }
+								   if(response.responseText == 101) {
+										Ext.Msg.alert('Error', 'Incorrect Captcha, please try again.');
+									}
+								}
+							});
+							
+							/*Ext.Ajax.request({
+								url: 'fdbck.php',
+								params: fieldValues,
+								success: function(response){
+									console.log(response);
+									//var text = response.responseText;
+									// process server response here
+									Ext.Msg.alert('Thank you!', 'Thank you, your message has been successfuly sent. We will get back to you as soon as possible.');
+									win.close();
+								},
+								failure: function(response){
+									console.log(response);
+									if(response.responseText == 101) {
+										Ext.Msg.alert('Error', 'Incorrect Captcha, please try again.');
+									}
+								}
+							});*/
+						}
+					}
 				},
 				{
 					id: 'feedbackCancel',
