7d5601c64ff471b00dbfce506bd3e2882467df6b
Franck <franck.o.perrin@free.fr>
2014-01-16 15:12:26 +0000

    Fix modify error where index would be wrong by using the ID instead

--- app/controller/Compos.js
+++ app/controller/Compos.js
@@ -301,7 +301,7 @@ Ext.define('WIDGaT.controller.Compos', {
 				
 					var indexOfParentComp = WIDGaT.activeWidget.components().indexOfId(record.get('widgat.model.compo_id'));
 										
-					var root = "components["+indexOfParentComp+"].attributes['"+record.get('shortName')+"']";
+					var root = "components['"+record.get('widgat.model.compo_id')+"'].attributes['"+record.get('shortName')+"']";
 
 					var tmpO = record.json4Serv();
 					tmpO.root = root;
